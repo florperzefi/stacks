80a219d3e6120cce8f3d4a97e0955ecddeba7c17
Flor Perzefi <flor.perzefi@lhind.dlh.de>
2025-06-16 09:59:20 +0200

    added stack files

---
+++ dev/foundation/index.ts
@@ -0,0 +1,11 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+// Create a GCP resource (Storage Bucket)
+const bucket = new gcp.storage.Bucket("foundation-bucket", {
+    location: "US"
+});
+
+// Export the DNS name of the bucket
+// change
+export const bucketName = bucket.url;
